53875d19560da68e8f16a6c56ead4d4eca6ef598
Donald Roshi <donald.roshi@klarna.com>
2024-12-29 18:08:39 +0100

    load from config file

---
+++ src/config.rs
@@ -0,0 +1,165 @@
+use ignore::gitignore::{Gitignore, GitignoreBuilder};
+use notify::{Error as NotifyError, Result as NotifyResult};
+use serde::de::Visitor;
+use serde::Deserialize;
+use std::{env::current_dir, error::Error, fs, path::PathBuf, process::Command};
+use std::{fmt::Debug, path::Path};
+
+#[derive(Debug)]
+pub struct GitignoreSerde(pub Gitignore);
+
+impl<'de> serde::Deserialize<'de> for GitignoreSerde {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct GitignorePathVisitor;
+
+        impl Visitor<'_> for GitignorePathVisitor {
+            type Value = GitignoreSerde;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("a path string to a .gitignore file")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                let root = find_root().unwrap_or_default();
+                let mut builder = GitignoreBuilder::new(&root);
+                builder.add(v);
+                let git = builder
+                    .build()
+                    .map_err(|err| E::custom(format!("Failed to build Gitignore: {err}")))?;
+
+                Ok(GitignoreSerde(git))
+            }
+        }
+
+        deserializer.deserialize_str(GitignorePathVisitor)
+    }
+}
+
+pub trait Extendable: Debug {
+    fn matcher(&self, path: &Path, is_dir: bool) -> bool;
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "extendable_type", content = "path")]
+pub enum ExtendableType {
+    #[serde(rename = "git")]
+    Git(GitignoreSerde),
+}
+
+impl Extendable for ExtendableType {
+    fn matcher(&self, path: &Path, is_dir: bool) -> bool {
+        match self {
+            ExtendableType::Git(wrapper) => wrapper
+                .0
+                .matched_path_or_any_parents(path, is_dir)
+                .is_ignore(),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    extend: Extend,
+    exclude: Exclude,
+    #[serde(default)]
+    root: PathBuf,
+}
+
+impl Config {
+    pub fn to_extend(&self) -> &Extend {
+        &self.extend
+    }
+
+    pub fn to_exclude(&self) -> &Exclude {
+        &self.exclude
+    }
+
+    pub fn root(&self) -> &PathBuf {
+        &self.root
+    }
+
+    fn setup(&mut self, root: PathBuf) {
+        let ignore_files: Vec<String> = self
+            .exclude
+            .files
+            .iter()
+            .map(|ignorable_path| ignorable_path.path.to_owned())
+            .collect();
+
+        self.root = root;
+        self.exclude.set_exclude_files(ignore_files);
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Extend {
+    extendables: Vec<ExtendableType>,
+}
+
+impl Extend {
+    pub fn to_extendables(&self) -> &Vec<ExtendableType> {
+        &self.extendables
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Exclude {
+    files: Vec<IgnorablePath>,
+    #[serde(default)]
+    exclude_files: Vec<String>,
+}
+
+impl Exclude {
+    pub fn to_exclude_files(&self) -> &Vec<String> {
+        &self.exclude_files
+    }
+
+    pub fn set_exclude_files(&mut self, ignore_files: Vec<String>) {
+        self.exclude_files = ignore_files;
+    }
+}
+
+#[derive(Debug, Deserialize)]
+pub struct IgnorablePath {
+    path: String,
+}
+
+pub fn load_config() -> Result<Config, Box<dyn Error>> {
+    let root = find_root()?;
+    let contents = fs::read_to_string(root.join("watchdog.toml")).map_err(|err| {
+        format!("An error occured while reading watchdog.toml config file: {err}")
+    })?;
+    let mut config: Config = toml::from_str(&contents).inspect_err(|err| {
+        eprintln!(
+            "An error occurred while deserialising watchdog.toml: {}",
+            err.message()
+        )
+    })?;
+
+    config.setup(root);
+    Ok(config)
+}
+
+fn find_root() -> NotifyResult<PathBuf> {
+    let mut root_path = String::new();
+    if let Ok(git_root_path) = Command::new("git")
+        .args(["rev-parse", "--show-toplevel"])
+        .output()
+    {
+        root_path += std::str::from_utf8(&git_root_path.stdout).unwrap().trim();
+    } else if let Ok(cargo_root_path) = current_dir() {
+        root_path += cargo_root_path.to_str().unwrap();
+    }
+
+    if root_path.is_empty() {
+        return Err(NotifyError::generic("Could not find the root project"));
+    }
+
+    Ok(PathBuf::from(root_path))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,41 @@
-use core::str;
-use notify::{Config, Error, Event, RecommendedWatcher, RecursiveMode, Result, Watcher};
+use config::{Config, Extendable};
+use notify::{
+    event::{CreateKind, ModifyKind, RemoveKind},
+    Config as NotifyConfig, Error, Event, EventKind, RecommendedWatcher, RecursiveMode, Result,
+    Watcher,
+};
 use std::{
-    env::{current_dir, var},
-    fs,
-    io::{BufRead, BufReader},
-    path::{Path, PathBuf},
+    env::var,
     process::{Child, Command},
     sync::mpsc::channel,
 };
 
 mod config;
-mod ignore;
 
 fn main() -> Result<()> {
-    let root = find_root()?;
-    let config =
-        config::load_config(&root.join("watchdog.toml")).unwrap_or(config::Config::empty());
+    let config = config::load_config().map_err(|err| Error::generic(&format!("{err}")))?;
 
-    let ignore_files = ignore::ignore_files(config);
-    println!("files to ignore: {ignore_files:?}");
     match var("WATCHDOG_CHILD_PROC") {
         Ok(_) => run_child(),
-        Err(_) => watchdog(&root),
+        Err(_) => watchdog(&config),
     }
 }
 
-fn find_root() -> Result<PathBuf> {
-    let mut root_path = String::new();
-    if let Ok(git_root_path) = Command::new("git")
-        .args(["rev-parse", "--show-toplevel"])
-        .output()
-    {
-        root_path += str::from_utf8(&git_root_path.stdout).unwrap().trim();
-    } else if let Ok(cargo_root_path) = current_dir() {
-        root_path += cargo_root_path.to_str().unwrap();
-    }
-
-    if root_path.is_empty() {
-        return Err(Error::generic("Could not find the root project"));
-    }
-
-    Ok(PathBuf::from(root_path))
-}
-
-fn watchdog(root_path: &PathBuf) -> Result<()> {
+fn watchdog(config: &Config) -> Result<()> {
+    let root_path = config.root();
     let (tx, rx) = channel();
 
     let mut child_proc = start_app();
 
-    let config = Config::default();
-    let mut watcher = RecommendedWatcher::new(tx, Config::with_compare_contents(config, true))?;
+    let notify_config = NotifyConfig::default();
+    let mut watcher =
+        RecommendedWatcher::new(tx, NotifyConfig::with_compare_contents(notify_config, true))?;
 
     watcher.watch(root_path, RecursiveMode::Recursive)?;
-    println!("Started watching directory {root_path:?}");
 
     for res in rx {
         match res {
-            Ok(event) => event_handler(event, &mut child_proc, root_path),
+            Ok(event) => event_handler(event, &mut child_proc, config),
             Err(e) => eprintln!("Watch error: {e:?}"),
         }
     }
@@ -85,45 +64,37 @@ fn handler(_: Event, proc: &mut Option<Child>) {
     *proc = start_app();
 }
 
-fn event_handler(event: Event, proc: &mut Option<Child>, root_path: &Path) {
-    let should_ignore = should_ignore_event(root_path, &event.paths);
+fn event_handler(event: Event, proc: &mut Option<Child>, config: &Config) {
+    let should_ignore = match &event.kind {
+        EventKind::Create(create_kind) => {
+            should_ignore_event(config, &event, create_kind == &CreateKind::Folder)
+        }
+        EventKind::Remove(remove_kind) => {
+            should_ignore_event(config, &event, remove_kind == &RemoveKind::Folder)
+        }
+        EventKind::Modify(ModifyKind::Data(_)) | EventKind::Modify(ModifyKind::Name(_)) => {
+            should_ignore_event(config, &event, false)
+        }
+        _ => true,
+    };
     if !should_ignore {
         handler(event, proc);
     }
 }
 
-fn get_ignorable_paths(root_path: &Path) -> Vec<String> {
-    let git = root_path.join(".git").to_str().unwrap().to_string();
-    let mut ignorable_paths = extend_git_ignore(root_path);
-    ignorable_paths.push(git);
-
-    ignorable_paths
-}
-
-fn extend_git_ignore(root_path: &Path) -> Vec<String> {
-    let path = root_path.join(".gitignore");
-    let exists = fs::exists(&path);
-    if exists.is_err() || !exists.unwrap() {
-        return vec![];
-    }
-    let file =
-        fs::File::open(&path).unwrap_or_else(|_| panic!("tried to open file at path {path:?}"));
-
-    let reader = BufReader::new(file);
-    let paths_to_ignore: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
-
-    paths_to_ignore
-}
-
-fn should_ignore_event(root_path: &Path, paths: &[PathBuf]) -> bool {
-    let paths_to_ignore = get_ignorable_paths(root_path);
+fn should_ignore_event(config: &Config, event: &Event, is_dir: bool) -> bool {
+    let paths = &event.paths;
+    let paths_to_ignore = config.to_exclude().to_exclude_files();
+    let extendables = &config.to_extend().to_extendables();
     paths.iter().all(|path| {
-        let path = path.to_str().unwrap();
-        for path_to_ignore in &paths_to_ignore {
-            if path.contains(path_to_ignore) {
+        for path_to_ignore in paths_to_ignore {
+            if path.to_str().unwrap().contains(path_to_ignore) {
                 return true;
             }
         }
-        false
+
+        extendables
+            .iter()
+            .any(|extendable| extendable.matcher(path, is_dir))
     })
 }
